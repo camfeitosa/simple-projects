1c8dbcaf3deaa14f8ffdb906010fbc068daaf35a
Camila Feitosa <114369214+camfeitosa@users.noreply.github.com>
2023-04-23 16:12:40 -0300

    five

--- auto-write-text/script.js
+++ auto-write-text/script.js
@@ -1,9 +1,18 @@
-const text = 'Utilize o Brainway como ferramenta para otimizar seus estudos'
+const text = 'Utilize o Brainway como ferramenta para organização!!!'
 
 let index = 0
 
 function writeText(){
     document.body.innerText = text.slice
+    (0, index)
+
+    index++ 
+
+    if(index > text.length){
+        index = 0
+    } 
 }
+
+setInterval(writeText, 100)
 
 
